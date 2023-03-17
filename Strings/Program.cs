16f7fee36aeaba14e062db864534e3db4b40ccc5
FellipiBorges <fellipiborgesmc@gmail.com>
2023-03-17 00:07:45 -0300

    Adicionar arquivos de projeto.

---
+++ Strings/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.WriteLine("--------      funções loucas       --------");
+string empresa = "               Microsoft Corporation    ";
+
+Console.WriteLine("TRIM - retira os espaços em branco antes e após a expressão");
+Console.WriteLine($"Nome sem espaços: {empresa.Trim()}");
+Console.WriteLine(empresa);
