2a7ecf2418f98ca07397f7c7647cd6dce2996653
FellipiBorges <fellipiborgesmc@gmail.com>
2023-03-17 00:15:44 -0300

    Correção Texto

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -1,7 +1,9 @@
 ﻿
-Console.WriteLine("--------      funções loucas       --------");
-string empresa = "               Microsoft Corporation    ";
+Console.WriteLine("--------      funções básicas       --------");
+string empresa = "               Microsoft Corporation         ";
 
 Console.WriteLine("TRIM - retira os espaços em branco antes e após a expressão");
-Console.WriteLine($"Nome sem espaços: {empresa.Trim()}");
-Console.WriteLine(empresa);
+
+Console.WriteLine("Essa linha é um teste sem a funcionalidade do TRIM" + empresa;
+
+Console.WriteLine($"Nome sem espaços e com funcionalidade TRIM: {empresa.Trim()}");
