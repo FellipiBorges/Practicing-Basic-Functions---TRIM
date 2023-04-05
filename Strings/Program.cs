2dfa7743a96181fa4a1e0c939017d3d50259ccb9
FellipiBorges <fellipiborgesmc@gmail.com>
2023-04-04 21:35:46 -0300

    Correção Texto

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -4,6 +4,8 @@ string empresa = "               Microsoft Corporation         ";
 
 Console.WriteLine("TRIM - retira os espaços em branco antes e após a expressão");
 
-Console.WriteLine("Essa linha é um teste sem a funcionalidade do TRIM" + empresa;
-
+Console.WriteLine("Essa linha é um teste sem a funcionalidade do TRIM" + empresa);
 Console.WriteLine($"Nome sem espaços e com funcionalidade TRIM: {empresa.Trim()}");
+
+Console.WriteLine("Length - retorna a quantidade de caracteres");
+Console.WriteLine($"Tamanho do texto: {empresa.Trim()}");
